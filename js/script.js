80666791d4f779a8b725b47ee56c53ba42599bf3
CarloMtzMartin <35570588+CarloMtzMartin@users.noreply.github.com>
2021-11-09 18:43:43 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function quesito() {
   fetch("https://foaas.com/awesome/carlo")
   .then(response => {
     //document.body.innerHTML = "<h1>"+ response.json() + "</h1>";
-    return reponse.json();
+    return response.json();
   }).then(x => {
     console.log({x});
     document.body.innerHTML = "<h1>"+ x + "</h1>";
