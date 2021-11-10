f3b63da8121753834f5ee38299de512f625a5d1c
CarloMtzMartin <35570588+CarloMtzMartin@users.noreply.github.com>
2021-11-09 19:46:59 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 let d = new Date();
 //document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
-setInterval(quesito, 1000);
+setInterval(quesito, 5000);
 
 function quesito() {
+  const d = new Date();
+  document.getElementById("time").innerHTML = d.toLocaleTimeString();
   fetch("https://uselessfacts.jsph.pl/random.json?language=en" , {
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
     //mode: 'cors', // no-cors, *cors, same-origin
