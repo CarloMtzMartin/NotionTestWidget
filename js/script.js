3a1f1f93217cd56240d7dbb289af05f9ffb6652d
CarloMtzMartin <35570588+CarloMtzMartin@users.noreply.github.com>
2021-11-09 18:40:11 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+//document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
 quesito();
 
 function quesito() {
   fetch("https://foaas.com/awesome/carlo")
   .then(response => {
-    return reponse.json();
+    document.body.innerHTML = "<h1>"+ response.json() + "</h1>";
+    //return reponse.json();
+  }).then(x => {
+    console.log({x});
+    document.body.innerHTML = "<h1>"+ x + "</h1>";
   });
 }
