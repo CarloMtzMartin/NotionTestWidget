19dee1cdfc7f1751a2a84ca6d9c59252444bef3c
CarloMtzMartin <35570588+CarloMtzMartin@users.noreply.github.com>
2021-11-09 18:36:34 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 let d = new Date();
 document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+quesito();
+
+function quesito() {
+  fetch("https://foaas.com/awesome/carlo")
+  .then(response => {
+    return reponse.json();
+  });
+}
